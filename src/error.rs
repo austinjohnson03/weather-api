f699e2b1c74cd2b1fce957cd8b36aec13f69c63a
austinjohnson03 <amj102998@gmail.com>
2026-05-04 07:13:28 -0400

    Created error enum for client
    Signed-off-by: austinjohnson03 <amj102998@gmail.com>

---
+++ src/error.rs
@@ -0,0 +1,26 @@
+#[derive(Debug, thiserror::Error)]
+pub enum ClientError {
+    #[error("Failed to build HTTP client: {0}")]
+    BuilderError(#[from] reqwest::Error),
+
+    #[error("Connection timeout")]
+    ConnectionTimeout,
+
+    #[error("Request time out")]
+    RequestTimeout,
+
+    #[error("HTTP error ({status}): {body}")]
+    HttpError { status: u16, body: String },
+
+    #[error("Too many redirects")]
+    TooManyRedirects,
+
+    #[error("Failed to parse JSON reponse: {0}")]
+    JsonParseError(#[from] serde_json::Error),
+
+    #[error("TLS Error: {0}")]
+    TlsError(String),
+
+    #[error("Unexpected Error: {0}")]
+    Unknown(String),
+}
